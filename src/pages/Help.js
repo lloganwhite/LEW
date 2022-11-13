24f81d5dd8bbd171136adb7c61c8a0a5532014be
lloganwhite <lew6@email.sc.edu>
2022-11-13 17:26:32 -0500

    Create Help.js

---
+++ src/pages/Help.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Help = () => {
+    return (
+        <div>
+            <h1>My Help Page</h1>
+            <p>This is my Help page</p>
+        </div>
+    )
+}
+export default Help
