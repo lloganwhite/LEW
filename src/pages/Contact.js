4b85da6e18570aeb6122e42ad8d5e30a48d12e72
lloganwhite <lew6@email.sc.edu>
2022-11-13 17:23:27 -0500

    Update index.css

---
+++ src/pages/Contact.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>My Contact Page</h1>
+            <p>This is my Contact page</p>
+        </div>
+    )
+}
+export default Contact
---
+++ src/pages/Gallery.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const Gallery = () => {
+    return (
+        <div>
+            <h1>My Gallery Page</h1>
+            <p>This is my Gallery page</p>
+        </div>
+    )
+}
+export default Gallery
