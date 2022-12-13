0e26e034e47758e92e89b8379b3a43530c6940b0
lloganwhite <lew6@email.sc.edu>
2022-12-12 23:53:07 -0500

    slay

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,14 @@ import React from "react"
 import Header from "./header"
 import BottomNav from "./bottomnav"
 
+
 const Layout = props => {
     return (
         <div>
             <Header />
             {props.children}
             <BottomNav/>
+
         </div>
     )
 }
